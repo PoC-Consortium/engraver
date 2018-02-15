c8aa46fae287701158bbdf81483d1f8b21e7a220
Jake-B <jake-b@users.noreply.github.com>
2018-02-14 20:44:34 -0500

    Fix bugs in the test.pl script cuased by my poor understanding of perl.

--- test.pl
+++ test.pl
@@ -12,32 +12,35 @@ my $expected =  "3ba11bee42182e0684df470d420c813e";
 
 print qx{$plotbin -a -v -k 11424087411148401423 -d core0 -x 0 -s 0 -n 128 -t 4};
 my $digest = `$md5sum core0/11424087411148401423_0_128_128`;
-$digest =~ /(([^@]+)@(\S+))/;
+chomp($digest);
+$digest =~ /^([^\s])/;
 if ($digest eq $expected) {
-	print "Digset did not match.  Expected $expected got $digest";
-	exit 1;
+	print "Digest OK\n"
 } else {
-	print "Digest OK"
+	print "Digset did not match.  Expected $expected got $digest\n";
+	exit 1;
 }
 
 print qx{$plotbin -a -v -k 11424087411148401423 -d core1 -x 1 -s 0 -n 128 -t 4};
 $digest = `$md5sum core1/11424087411148401423_0_128_128`;
-$digest =~ /(([^@]+)@(\S+))/;
+chomp($digest);
+$digest =~ /^([^\s])/;
 if ($digest eq $expected) {
-	print "Digest did not match.  Expected $expected got $digest";
-	exit 1;
+	print "Digest OK\n"
 } else {
-	print "Digest OK"
+	print "Digest did not match.  Expected $expected got $digest\n";
+	exit 1;
 }
 
 
 print qx{$plotbin -a -v -k 11424087411148401423 -d core2 -x 2 -s 0 -n 128 -t 4};
 $digest = `$md5sum core2/11424087411148401423_0_128_128`;
-$digest =~ /(([^@]+)@(\S+))/;
+chomp($digest);
+$digest =~ /^([^\s])/;
 if ($digest eq $expected) {
-	print "Digest did not match.  Expected $expected got $digest";
-	exit 1;
+	print "Digest OK\n"
 } else {
-	print "Digest OK"
+	print "Digest did not match.  Expected $expected got $digest\n";
+	exit 1;
 }
 
